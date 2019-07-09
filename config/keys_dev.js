4c903dcc5d3f331d4ef9f143657d7eaa3cd507a0
mustak <mustakhridoy@gmail.com>
2019-07-10 04:23:53 +0600

    env

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-    DATABASE: 'mongodb+srv://mustak123:mustak123@cluster0-n2lsy.mongodb.net/test?retryWrites=true&w=majority',
-    SECRET: 'SECRET'
+    DATABASE: process.env.DATABASE,
+    SECRET: SECRET
   };
   
