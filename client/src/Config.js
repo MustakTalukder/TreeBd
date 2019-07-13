6dd4928c816ca17c3d21c94830a94a9a004abb43
mustak <mustakhridoy@gmail.com>
2019-07-14 04:24:46 +0600

    client API change with PORT

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,4 +1,7 @@
 
-export const API = process.env.REACT_APP_API_URL
+// export const API = process.env.REACT_APP_API_URL
+
+export const API = `${process.env.PORT}/api`
+
 
 // export const API = process.env.REACT_APP_API_URL;
