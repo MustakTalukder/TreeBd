fcc2319829f945799a1dcfb33e7a17c66ebf0c08
mustak <mustakhridoy@gmail.com>
2019-07-14 22:40:42 +0600

    fix sign up

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,86 +9,49 @@ const ResetTemplate = require('../emailTemplate/ResetTemplate')
 require('dotenv').config()
 
 
-exports.signup = async (req, res) => {
+exports.signup =  (req, res) => {
 
-    console.clear()
-
+    
     try {
-        //const user = new User(req.body);
+        const user = new User(req.body);
 
-       // user.activateToken = jwt.sign({ _id: user._id, name: user.name, email: user.email, password: user.password }, 'process.env.SECRET', { expiresIn: '1d' }) 
+        user.activateToken = jwt.sign({ _id: user._id, name: user.name, email: user.email, password: user.password }, 'process.env.SECRET', { expiresIn: '1d' }) 
     
-
-        let activateLink = `tesing activation`
-        
-        let template = verificationTemplate({
-            name: req.body.name,
-            link: activateLink
-        })
-
-        await transporter.sendMail({
-            from: "learnandexploreteam@gmail.com",
-            to: req.body.email,
-            subject: "TreeBd||Please Verify your account",
-            html: template
-        });
-        
-        res.json({
-            email: "Email found"
-
-        });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // user.save(async (err, user) => {          
-        //     console.log("user==", user);
-
-        //     if (err) {
     
-        //         return res.status(400).json({
-        //             err: errorHandler(err)
-        //         })
-        //     }
+        user.save(async (err, user) => {
+
+            if (err) {
+    
+                return res.status(400).json({
+                    err: errorHandler(err)
+                })
+            }
 
     
-        //     // Mail verificatio
+            // Mail verificatio
     
-        //     let activateLink = `/activateaccount/${user._id}?token=${user.activateToken}`
+            let activateLink = `/activateaccount/${user._id}?token=${user.activateToken}`
             
-        //     let template = verificationTemplate({
-        //         name: user.name,
-        //         link: activateLink
-        //     })
+            let template = verificationTemplate({
+                name: user.name,
+                link: activateLink
+            })
     
-        //     await transporter.sendMail({
-        //         from: "learnandexploreteam@gmail.com",
-        //         to: user.email,
-        //         subject: "TreeBd||Please Verify your account",
-        //         html: template
-        //     });
-    
-    
-        //     // user.salt = undefined
-        //     // user.hashed_password = undefined
-    
-        //     res.json({
-        //          user
-        //      });
-        // })
+            await transporter.sendMail({
+                from: "learnandexploreteam@gmail.com",
+                to: user.email,
+                subject: "TreeBd||Please Verify your account",
+                html: template
+            });
     
     
+            // user.salt = undefined
+            // user.hashed_password = undefined
     
+            res.json({
+                 user
+             });
+        })
         
     } catch (err) {
         
